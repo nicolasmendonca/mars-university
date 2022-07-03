0d093e5c44839223cbd1061fbd5224c2e87ae81b
Niko Mendo <nicolasmendonca@gmail.com>
2022-07-03 14:12:49 +0000

    #4: Create lesson page

---
+++ src/features/lessonDetails/application/svelte/components/LessonDetails.svelte
@@ -0,0 +1,61 @@
+<div class="p-4">
+	<div class="p-4 rounded-md bg-neutral-200 text-black">
+        <nav>
+            <a href="#" class="text-accent-800 underline">&larr; Back to course</a>
+        </nav>
+        <div class="my-4" />
+		<article class="grid gap-4 grid-flow-row">
+			<img
+				class="mx-auto rounded-md shadow-md"
+				height="400"
+				width="600"
+				src="https://picsum.photos/600/400"
+				alt=""
+			/>
+			<h1 class="text-2xl font-bold">Getting Started with Sveltekit</h1>
+
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum hic esse corrupti earum atque
+				vero ipsa temporibus impedit corporis, dignissimos doloremque, mollitia illo dolor nihil
+				aperiam officiis incidunt quibusdam aliquam.
+			</p>
+			<p>
+				Lorem ipsum dolor sit, <b>amet consectetur adipisicing elit.</b> Possimus laborum consectetur,
+				quisquam aliquam veniam incidunt obcaecati beatae recusandae ipsam placeat in adipisci molestias
+				optio eius pariatur at quidem rerum hic!
+			</p>
+		</article>
+		<div class="my-4" />
+		<nav class="flex justify-between">
+			<a href="#" class="text-accent-800 underline">&larr; Previous Lesson</a>
+			<a href="#" class="text-accent-800 underline">Next Lesson &rarr;</a>
+		</nav>
+	</div>
+
+	<div class="my-8" />
+
+	<nav aria-labelledby="course-content-title">
+		<h2 id="course-content-title" class="text-2xl font-bold">Course Content</h2>
+
+		<div class="my-4">
+			<section class="flex flex-col shadow">
+				<h3 class="p-2 text-white bg-accent-800 text-lg font-semibold rounded-t-lg">Databases</h3>
+				<a
+					href="#"
+					class="p-2 bg-accent-600 hover:underline transition-all hover:bg-accent-700 text-accent-100"
+					>Microsoft SQL Server - 10+ years</a
+				>
+				<a
+					href="#"
+					class="p-2 bg-accent-600 hover:underline transition-all hover:bg-accent-700 text-accent-100"
+					>Sybase SQL Server - 5 years</a
+				>
+				<a
+					href="#"
+					class="p-2 bg-accent-600 hover:underline transition-all hover:bg-accent-700 text-accent-100 rounded-b-lg"
+					>Oracle - 5 years</a
+				>
+			</section>
+		</div>
+	</nav>
+</div>
---
+++ src/routes/lesson.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import LessonDetails from '$features/lessonDetails/application/svelte/components/LessonDetails.svelte'
+</script>
+
+<LessonDetails />
